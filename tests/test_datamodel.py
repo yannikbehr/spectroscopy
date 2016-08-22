eba98d07dc7adab66bbc8ed6061972c99e6391a4
Yannik Behr <yannik@yanmail.de>
2016-08-22 23:24:34 +0200

    First implementation of the data model

--- tests/test_datamodel.py
+++ tests/test_datamodel.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from spectroscopy import dataset
+from spectroscopy.dataset import Dataset, Spectra
 
 class DatamodelTestCase(unittest.TestCase):
 
@@ -10,13 +10,22 @@ class DatamodelTestCase(unittest.TestCase):
         pass
 
     def test_ram_plugin(self):
-        d = dataset.Dataset.new('ram')
-        d._root.create_item('spectra/someid/counts', np.zeros((1, 2048)))
+        d = Dataset.new('ram')
+        p = d.plugin
+        p.create_item('spectra/someid/counts', np.zeros((1, 2048)))
         self.assertTrue(d['spectra/someid/counts'].shape == (1, 2048))
         self.assertTrue(np.alltrue(d['spectra/someid/counts'] < 1))
         d['spectra/someid/counts'] = np.ones((1, 2048))
         self.assertFalse(np.alltrue(d['spectra/someid/counts'] < 1))
 
+    def test_spectra(self):
+        d = Dataset.new('ram')
+        s = Spectra(d.plugin, counts=np.zeros((1, 2048)))
+        self.assertTrue(np.alltrue(s.counts < 1))
+        s.angle = np.array([45.0])
+        self.assertTrue(s.angle[0] == 45.0)
+        d.spectra.append(s.resource_id.id)
+        print d.spectra
 
 def suite():
     return unittest.makeSuite(DatamodelTestCase, 'test')
