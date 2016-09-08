8349071be1c6326af87182df4f97e8787c30edc9
Yannik Behr <yannikbehr@yanmail.de>
2016-09-08 12:40:28 +1200

    adding test for flyspec plugin

---
+++ tests/test_flyspecplugin.py
@@ -0,0 +1,115 @@
+import inspect
+import os
+import unittest
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import binned_statistic
+
+from spectroscopy.dataset import Dataset, ResourceIdentifier
+from spectroscopy.plugins.flyspec import FlySpecPlugin
+
+
+class FlySpecPluginTestCase(unittest.TestCase):
+    """
+    Test plugin to read FlySpec data.
+    """
+
+    def setUp(self):
+        self.data_dir = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), "data")
+
+    def test_open(self):
+        d = Dataset.open(os.path.join(self.data_dir,
+                                      '2012_02_29_1340_CHILE.txt'),
+                         format='FLYSPEC')
+        s = d.spectra[0]
+        self.assertEqual(s.time.shape,(4600,))
+        self.assertEqual(s.angle[0],135.140)
+        r = d.retrievals[3]
+        s1 = ResourceIdentifier(r.spectra_id).get_referred_object()
+        angle = s1.angle[r.slice]
+        id_max = np.argmax(r.sca)
+        np.testing.assert_almost_equal(angle[id_max], 168.04, 2)
+        self.assertEqual(len(d.retrievals), 36)
+        
+        # dicretize all retrievals onto a grid to show a daily plot
+        bins = np.arange(0,180,1.0)
+        nretrieval = len(d.retrievals)
+        m = np.zeros((nretrieval,bins.size-1))
+        for i,_r in enumerate(d.retrievals):
+            _s = ResourceIdentifier(_r.spectra_id).get_referred_object()
+            _angle = _s.angle[_r.slice]
+            _so2 = _r.sca
+            _so2_binned = binned_statistic(_angle, _so2, 'mean', bins)
+            m[i,:] = _so2_binned.statistic
+        ids = np.argmax(np.ma.masked_invalid(m),axis=1)
+        maxima = np.array([166., 167., 167., 167., 168., 167., 168., 167.,
+                            167., 167., 167., 167., 168., 167., 167., 167.,
+                            167., 166., 167., 166., 166., 167., 165., 165.,
+                            165., 164., 165., 163., 163., 164., 163., 165.,
+                            164., 164., 164., 161.])
+        np.testing.assert_array_almost_equal(maxima, bins[ids], 2)
+        #plt.contourf(range(nretrieval),bins[1:],m.T,100)
+        #plt.contour(range(nretrieval),bins[1:],m.T,100)
+        #plt.ylim(140,175)
+        #plt.show()
+
+    def test_split_by_scan(self):
+        f = FlySpecPlugin()
+        angles = np.array([30, 35, 40, 35, 30, 35, 40])
+        result = [np.array([30, 35, 40]), np.array([30, 35]),
+                  np.array([35, 40])]
+        for i,a in enumerate(f._split_by_scan(angles)):
+            np.testing.assert_array_equal(a[0],result[i])
+
+        result1 = [np.array([1, 2, 3]), np.array([5, 4]), np.array([6, 7])]
+        for i,a in enumerate(f._split_by_scan(angles, np.array([1,2,3,4,5,6,7]))):
+            np.testing.assert_array_equal(a[1],result1[i])
+
+        angles1 = np.array([30, 30, 35, 40, 35, 30, 35, 40, 40])
+        result2 = [np.array([30, 30, 35, 40]), np.array([30, 35]),
+                  np.array([35, 40, 40])]
+        for i,a in enumerate(f._split_by_scan(angles1)):
+            np.testing.assert_array_equal(a[0],result2[i])
+
+        angles2 = np.array([30, 35, 40, 45, 30, 35, 40, 45])
+        result3 = [np.array([30, 35, 40, 45]), 
+                   np.array([30, 35, 40, 45])]
+        for i,a in enumerate(f._split_by_scan(angles2)):
+            np.testing.assert_array_equal(a[0],result3[i])
+
+        angles3 = np.array([30., 35., 40., 40., 45., 30., 35., 40., 45.])
+        result4 = [np.array([30, 35, 40, 40, 45]), 
+                   np.array([30, 35, 40, 45])]
+        for i,a in enumerate(f._split_by_scan(angles3)):
+            np.testing.assert_array_equal(a[0],result4[i])
+
+        angles4 = np.array([30, 35, 40, 40, 40, 45, 30, 35, 40, 45])
+        with self.assertRaises(ValueError):
+            [a for a in f._split_by_scan(angles4)]
+            
+        angles5 = np.array([174.750, 174.750, 174.420, 174.090, 173.750,
+                            173.420, 173.080, 172.750, 172.420, 172.080,
+                            171.750, 171.750, 171.410, 171.080, 170.740])
+        result5 = [angles5[::-1]]
+        for i,a in enumerate(f._split_by_scan(angles5)):
+            np.testing.assert_array_equal(a[0],result5[i])
+        
+
+    def test_array_multi_sort(self):
+        f = FlySpecPlugin()
+        x1 = np.array([4.,5.,1.,2.])
+        x2 = np.array([10.,11.,12.,13.])
+        result = (np.array([ 1.,  2.,  4.,  5.]),
+                  np.array([ 12.,  13.,  10.,  11.]))
+        out = f._array_multi_sort(*tuple([x1,x2]))
+        np.testing.assert_array_equal(out[0], result[0])
+        np.testing.assert_array_equal(out[1], result[1])
+        
+
+def suite():
+    return unittest.makeSuite(FlySpecPluginTestCase, 'test')
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
