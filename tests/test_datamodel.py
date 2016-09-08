8a9098b253fcf2f7ace883d48ea2be8012d7f19a
Yannik Behr <yannikbehr@yanmail.de>
2016-09-08 12:35:04 +1200

    minor changes due to changes in dataset

--- tests/test_datamodel.py
+++ tests/test_datamodel.py
@@ -1,14 +1,17 @@
+import inspect
+import os
 import unittest
 
 import numpy as np
 
 from spectroscopy.dataset import Dataset, Spectra
 
+
 class DatamodelTestCase(unittest.TestCase):
 
     def setUp(self):
         pass
-
+    
     def test_ram_plugin(self):
         d = Dataset.new('ram')
         p = d.plugin
@@ -17,15 +20,15 @@ class DatamodelTestCase(unittest.TestCase):
         self.assertTrue(np.alltrue(d['spectra/someid/counts'] < 1))
         d['spectra/someid/counts'] = np.ones((1, 2048))
         self.assertFalse(np.alltrue(d['spectra/someid/counts'] < 1))
-
+        
     def test_spectra(self):
         d = Dataset.new('ram')
         s = Spectra(d.plugin, counts=np.zeros((1, 2048)))
         self.assertTrue(np.alltrue(s.counts < 1))
         s.angle = np.array([45.0])
         self.assertTrue(s.angle[0] == 45.0)
-        d.spectra.append(s.resource_id.id)
-        print d.spectra
+        d.spectra.append(s)
+
 
 def suite():
     return unittest.makeSuite(DatamodelTestCase, 'test')
