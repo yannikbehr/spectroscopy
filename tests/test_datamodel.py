b3c1929f8dfffce9258e3d712a044a7776386a1e
Yannik Behr <yannikbehr@yanmail.de>
2016-09-09 16:02:24 +1200

    tests for __add__ and __iadd__ operators

--- tests/test_datamodel.py
+++ tests/test_datamodel.py
@@ -1,5 +1,3 @@
-import inspect
-import os
 import unittest
 
 import numpy as np
@@ -11,7 +9,7 @@ class DatamodelTestCase(unittest.TestCase):
 
     def setUp(self):
         pass
-    
+
     def test_ram_plugin(self):
         d = Dataset.new('ram')
         p = d.plugin
@@ -20,14 +18,31 @@ class DatamodelTestCase(unittest.TestCase):
         self.assertTrue(np.alltrue(d['spectra/someid/counts'] < 1))
         d['spectra/someid/counts'] = np.ones((1, 2048))
         self.assertFalse(np.alltrue(d['spectra/someid/counts'] < 1))
-        
+
     def test_spectra(self):
         d = Dataset.new('ram')
         s = Spectra(d.plugin, counts=np.zeros((1, 2048)))
         self.assertTrue(np.alltrue(s.counts < 1))
         s.angle = np.array([45.0])
         self.assertTrue(s.angle[0] == 45.0)
-        d.spectra.append(s)
+
+    def test_sum(self):
+        d1 = Dataset.new('ram')
+        s = Spectra(d1.plugin, counts=np.zeros((1, 2048)))
+        d1.spectra.append(s)
+        d2 = Dataset.new('ram')
+        d2.spectra.append(s)
+        d3 = d1 + d2
+        self.assertEqual(len(d3.spectra), 2)
+        self.assertTrue(d3 != d2)
+        self.assertTrue(d3 != d1)
+        self.assertEqual(d3.spectra[0], d3.spectra[1])
+        self.assertEqual(d3.spectra[0].counts.shape, (1, 2048))
+        with self.assertRaises(TypeError):
+            d4 = d1 + s
+        d5 = Dataset.new('ram')
+        d5 += d1
+        self.assertEqual(d5.spectra[0], d1.spectra[0])
 
 
 def suite():
