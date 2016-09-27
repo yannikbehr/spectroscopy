965a93eac0376d22370d1047b975f9cba334c2df
Yannik Behr <yannikbehr@yanmail.de>
2016-09-27 15:55:50 +1300

    pep8

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ from setuptools.command.install import install
 
 # do the build/install
 setup(
-      name="spectroscopy",
-      version="0.1",
-      description="Python package for processing field spectroscopy data.",
-      long_description="Python package for processing field spectroscopy data.",
-      author="Nial Peters",
-      author_email="nonbiostudent@hotmail.com",
-      url="",
-      license="GPL v3",
-      package_dir={'':'src'},
-      install_requires = ['pyinotify'],
-      packages=['spectroscopy', 'spectroscopy.flux', 'spectroscopy.doas',
-                'spectroscopy.plugins'],
-      package_data={"spectroscopy.flux":["flyspec_config.cfg"]},
-      )
+    name="spectroscopy",
+    version="0.1",
+    description="Python package for processing field spectroscopy data.",
+    long_description="Python package for processing field spectroscopy data.",
+    author="Nial Peters",
+    author_email="nonbiostudent@hotmail.com",
+    url="",
+    license="GPL v3",
+    package_dir={'': 'src'},
+    install_requires=['pyinotify'],
+    packages=['spectroscopy', 'spectroscopy.flux', 'spectroscopy.doas',
+                'spectroscopy.plugins', 'spectroscopy.util'],
+    package_data={"spectroscopy.flux": ["flyspec_config.cfg"]},
+)
