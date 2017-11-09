f8db4958f64f9b8ffbbdc7ec2e3fd2eaa06a0fd1
Yannik Behr <yannikbehr@yanmail.de>
2017-11-09 15:34:05 +1300

    initial commit of miniDOAS plugin + tests

---
+++ src/spectroscopy/plugins/minidoas.py
@@ -0,0 +1,206 @@
+"""
+Plugin to read MiniDOAS data.
+"""
+import calendar
+import codecs
+import datetime
+import os
+import struct
+
+import numpy as np
+import pyproj
+
+from spectroscopy.datamodel import (RawDataBuffer, 
+                                    ConcentrationBuffer,
+                                    RawDataTypeBuffer,
+                                    FluxBuffer,
+                                    MethodBuffer,
+                                    GasFlowBuffer)
+from spectroscopy.plugins import DatasetPluginBase, DatasetPluginBaseException
+from spectroscopy.util import bearing2vec
+
+class MiniDoasException(DatasetPluginBaseException):
+    pass
+
+
+class MiniDoasRaw(DatasetPluginBase):
+
+    def read(self, dataset, filename, timeshift=0.0, **kargs):
+        
+        bearing=None
+        try:
+            bearing = kargs['bearing']
+        except KeyError:
+            pass
+        else:
+            bearing = np.ones(data.shape[0])*bearing
+
+        fh = codecs.open(filename, encoding='utf-8-sig')
+        dt = np.dtype([('station', 'S2'), ('date', 'S10'), ('time', np.float),
+                       ('stept', np.int), ('angle', np.float), ('intt', np.int),
+                       ('nspec', np.int), ('specin', np.float), 
+                       ('counts', np.int, (482,))])
+        data = np.loadtxt(fh, converters={1: lambda x: '%s-%s-%s' %(x[0:4], x[4:6], x[6:8])},
+                          dtype=dt, delimiter=',')
+        # Construct datetimes
+        date = data['date'].astype('datetime64')
+        hours = (data['time']/3600.).astype(int)
+        minutes = ((data['time'] - hours*3600.)/60.).astype(int)
+        seconds = (data['time'] - hours*3600. - minutes*60.).astype(int)
+        mseconds = np.round((data['time'] - hours*3600. - minutes*60. - seconds), 3)*1e3
+        datetime = date + hours.astype('timedelta64[h]') + minutes.astype('timedelta64[m]') \
+        + seconds.astype('timedelta64[s]') + mseconds.astype('timedelta64[ms]')
+        datetime -= np.timedelta64(int(timeshift), 'h')
+
+        # Convert radians to decimal degrees
+        angles = data['angle']*360./(2.*np.pi)
+        rdtb = RawDataTypeBuffer(d_var_unit='ppm-m', 
+                                 ind_var_unit='nm',
+                                 name='measurement',
+                                 acquisition='stationary')
+        wavelengths = np.arange(30,512)
+        rb = RawDataBuffer(inc_angle=angles,
+                           datetime=datetime.astype(str),
+                           ind_var=wavelengths,
+                           d_var=data['counts'],
+                           integration_time=data['intt'])
+                            
+        return {str(rb):rb, str(rdtb):rdtb}
+ 
+        
+    @staticmethod
+    def get_format():
+        return 'minidoas-raw'
+
+
+class MiniDoasSpectra(DatasetPluginBase):
+
+    def read(self, dataset, filename, timeshift=0.0, **kargs):
+        try:
+            date=kargs['date']
+        except KeyError:
+            raise MiniDoasException("'date' unspecified.")
+        
+        dt = np.dtype([('datetime', 'S26'),('angle', np.float), ('value', np.float),
+               ('model_value', np.float), ('fitcoeff', np.float),
+               ('fitcoeff_err', np.float), ('fitshift', np.float),
+               ('fitshift_err', np.float), ('fitsqueeze', np.float),
+               ('fitsqueezeerr', np.float)])
+        data = np.loadtxt(filename, delimiter=',', skiprows=1, converters={0: lambda x: date+'T'+x},
+                          dtype=dt)
+        cb = ConcentrationBuffer(value=data['value'],
+                                 datetime=data['datetime'].astype('datetime64[ms]').astype(str),
+                                 gas_species='SO2',
+                                 unit='ppm-m')
+        return {str(cb):cb}
+        
+
+    @staticmethod
+    def get_format():
+        return 'minidoas-spectra'
+    
+class MiniDoasScan(DatasetPluginBase):
+    def __init__(self):
+        # setup projection from NZGD49 (NZMG) to WGS84
+        self.destp = pyproj.Proj(init="epsg:4326")
+        self.srcp = pyproj.Proj(init="epsg:27200")
+
+    def _plumegeometry2gasflow(self, pheight, pwidth, peasting, pnorthing,
+                               ptrack, datetime):
+        position = []
+        time = []
+        vx = []
+        vy = []
+        vz = []
+        for h,w,e,n,t,dt in zip(pheight,pwidth,peasting,pnorthing,ptrack, datetime):
+            lon,lat = pyproj.transform(self.srcp, self.destp, e, n)
+            position.append([lon, lat, h-w/2.])
+            position.append([lon, lat, h])
+            position.append([lon, lat, h+w/2.])
+            time.extend([dt]*3)
+            x,y = bearing2vec(t)
+            vx.extend([x]*3)
+            vy.extend([y]*3)
+            vz.extend([np.nan]*3)
+
+        description = 'Plume velocity inferred from plume geometry and wind speed'
+        mb = MethodBuffer(name='WS2PV', description=description)
+        gfb = GasFlowBuffer(vx=vx, vy=vy, vz=vz,
+                            position=position, datetime=time, 
+                            unit='m/s')
+        return (mb, gfb) 
+
+    def read(self, dataset, filename, **kargs):
+        try:
+            date=kargs['date']
+        except KeyError:
+            raise MiniDoasException("'date' unspecified.")
+    
+        dt = np.dtype([('time','S19'), ('ws', np.float), ('wd', np.float), 
+                   ('R2', np.float), ('SO2Start', np.float), ('SO2Max', np.float),
+                   ('SO2End', np.float), ('PlumeRange', np.float),
+                   ('PlumeWidth', np.float), ('PlumeHeight', np.float),
+                   ('Easting', np.float), ('Northing', np.float), ('Track', np.float),
+                   ('Emission', np.float), ('Station', 'S2'), ('EmissionSE', np.float)])
+        data = np.loadtxt(filename, delimiter=',', skiprows=1, dtype=dt, converters={0: lambda x: date+'T'+x})
+        fb = FluxBuffer(value=data['Emission'], value_error=data['EmissionSE'],
+                        datetime=data['time'].astype('datetime64[s]').astype(str))
+        mb, gfb = self._plumegeometry2gasflow(data['PlumeHeight'], data['PlumeWidth'],
+                                              data['Easting'], data['Northing'], 
+                                              data['Track'], data['time'])
+        return {str(fb):fb, str(mb):mb, str(gfb):gfb}
+
+    @staticmethod
+    def get_format():
+        return 'minidoas-scan'
+
+class MiniDoasWind(DatasetPluginBase):
+
+    def read(self, dataset, filename, **kargs):
+        try:
+            fn_wd = filename['direction']
+            fn_ws = filename['speed']
+        except KeyError:
+            msg = "Plugin expects as filename a dictionary of the form:"
+            msg += "{'direction': /path/to/wind-direction-file, "
+            msg += "'speed': /path/to/wind-speed-file}"
+            raise MiniDoasException(msg)
+
+        def dateconverter(x):
+            return datetime.datetime.strptime(x,'%d/%m/%Y %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S')
+
+        data1 = np.loadtxt(fn_wd, skiprows=1, dtype=np.dtype([('datetime','S19'), ('direction', np.float)]), delimiter='\t',
+                  converters={0:dateconverter}) 
+        data2 = np.loadtxt(fn_ws, skiprows=1, dtype=np.dtype([('datetime','S19'), ('speed', np.float)]), delimiter='\t',
+                  converters={0:dateconverter}) 
+
+        npts = data1.shape[0]
+        if npts != data2.shape[0]:
+            msg = "Wind direction and wind speed file don't "
+            msg += "have the same number of entries"
+            raise MiniDoasException(msg)
+        vx = np.zeros(npts)
+        vy = np.zeros(npts)
+        vz = np.zeros(npts)
+        for i in range(npts):
+            wd = data1['direction'][i]
+            ws = data2['speed'][i]
+            # if windspeed is 0 give it a tiny value
+            # so that the bearing can be reconstructed
+            if ws == 0.:
+                ws = 0.0001
+            _vx, _vy = bearing2vec(wd, ws)
+            vx[i] = _vx
+            vy[i] = _vy
+            vz[i] = np.nan
+        description = 'Autonomous weather station operated by NZ metservice'
+        mb = MethodBuffer(name='AWS', description=description)
+        m = dataset.new(mb)
+        gfb = GasFlowBuffer(methods=[m], vx=vx, vy=vy, vz=vz,
+                            datetime=data1['datetime'], unit='m/s')
+        return {str(gfb): gfb}
+
+    @staticmethod
+    def get_format():
+        return 'minidoas-wind'
+
---
+++ tests/test_minidoasplugin.py
@@ -0,0 +1,178 @@
+import inspect
+import os
+import tempfile
+import unittest
+
+import numpy as np
+
+from spectroscopy.dataset import Dataset
+from spectroscopy.plugins.minidoas import MiniDoasException
+from spectroscopy.visualize import plot
+from spectroscopy.datamodel import (PreferredFluxBuffer,
+                                    InstrumentBuffer,
+                                    TargetBuffer,
+                                    MethodBuffer)
+from spectroscopy.util import vec2bearing, bearing2vec
+
+
+class MiniDoasPluginTestCase(unittest.TestCase):
+    """
+    Test plugin to read FlySpec data.
+    """
+
+    def setUp(self):
+        self.data_dir = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), "data")
+
+    def test_read(self):
+        d = Dataset(tempfile.mktemp(), 'w')
+        e = d.read(os.path.join(self.data_dir, 'minidoas', 'NE_20161101.csv'),
+                        ftype='minidoas-raw')
+        rb = e['RawDataBuffer']
+        self.assertEqual(rb.d_var.shape, (7615, 482)) 
+        self.assertEqual(rb.d_var[0,0], 78)
+        self.assertEqual(rb.datetime[0], '2016-11-01T09:00:00.070000')
+        self.assertEqual(rb.datetime[-1], '2016-11-01T16:29:54.850000')
+        
+        with self.assertRaises(MiniDoasException):
+            e1 = d.read(os.path.join(self.data_dir, 'minidoas', 'NE_2016_11_01_Spectra.csv'),
+                        ftype='minidoas-spectra')
+        e1 = d.read(os.path.join(self.data_dir, 'minidoas', 'NE_2016_11_01_Spectra.csv'),
+                    date='2016-11-01', ftype='minidoas-spectra')
+        cb = e1['ConcentrationBuffer']
+
+        fn_wd = os.path.join(self.data_dir, 'minidoas', 'wind', '20161101_WD_00.txt')
+        fn_ws = os.path.join(self.data_dir, 'minidoas', 'wind', '20161101_WS_00.txt')
+
+        e2 = d.read({'direction': fn_wd, 'speed': fn_ws}, ftype='minidoas-wind')
+        gfb = e2['GasFlowBuffer']
+        self.assertEqual(int(vec2bearing(gfb.vx[0], gfb.vy[0])), 240)
+        self.assertAlmostEqual(np.sqrt(gfb.vx[0]**2 + gfb.vy[0]**2), 3.2, 1)
+        self.assertEqual(gfb.datetime[0], "2016-11-01T08:10:00")
+
+    def test_readall(self):
+        """
+        Produce a complete HDF5 file for 1 day of MiniDOAS analysis at one station.
+        """
+        d = Dataset('/tmp/minidoas_test.h5', 'w')
+        # Read the raw data
+        e0 = d.read(os.path.join(self.data_dir, 'minidoas', 'NE_20161101.csv'),
+                     ftype='minidoas-raw')
+        ib = InstrumentBuffer(name='WI301',
+                              location='White Island North-East Point',
+                              no_bits=16,
+                              type='MiniDOAS')
+        i = d.new(ib)
+        # ToDo: get correct plume coordinates
+        tb = TargetBuffer(name='White Island main plume',
+                          target_id='WI001',
+                          position=[-177.18375770, 37.52170799, 321.0])
+        t = d.new(tb)
+        rdt = d.new(e0['RawDataTypeBuffer'])
+        rb = e0['RawDataBuffer']
+        rb.type = rdt
+        rb.instrument = i
+        rb.target = t
+        lat = np.ones(rb.d_var.shape[0])*-37.516690
+        lon = np.ones(rb.d_var.shape[0])*177.1929793
+        elev = np.ones(rb.d_var.shape[0])*30.0
+        rb.position = np.array([lon, lat, elev]).T
+        rb.inc_angle_error = np.ones(rb.d_var.shape[0])*0.013127537*180./np.pi
+        rr = d.new(rb)
+
+        # Read the concentration
+        e1 = d.read(os.path.join(self.data_dir, 'minidoas', 'NE_2016_11_01_Spectra.csv'),
+                     date='2016-11-01', ftype='minidoas-spectra')
+        cb = e1['ConcentrationBuffer']
+        wpoptions = "{'Pixel316nm':479, 'TrimLower':30, 'LPFilterCount':3, 'MinWindSpeed':3,"
+        wpoptions += "'BrightEnough':500, 'BlueStep':5, 'MinR2:0.8, 'MaxFitCoeffError':50.0,"
+        wpoptions += "'InPlumeThresh':0.05, 'MinPlumeAngle':0.1, 'MaxPlumeAngle':3.0,"
+        wpoptions += "'MinPlumeSect':0.4, 'MaxPlumeSect':2.0, 'MeanPlumeCtrHeight':310,"
+        wpoptions += "'SEMeanPlumeCtrHeight':0.442, 'MaxRangeToPlume':5000, 'MaxPlumeWidth':2600"
+        wpoptions += "'MaxPlumeCentreAltitude':2000, 'MaxRangeSeperation':5000,"
+        wpoptions += "'MaxAltSeperation':1000, 'MaxTimeDiff':30,"
+        wpoptions += "'MinTriLensAngle':0.1745, 'MaxTriLensAngle':2.9671,"
+        wpoptions += "'SEWindSpeed':0.20, 'WindMultiplier':1.24, 'SEWindDir':0.174}"
+        mb1 = MethodBuffer(name='WidPro v1.2',
+                           description='Jscript wrapper for DOASIS',
+                           settings=wpoptions)
+        m1 = d.new(mb1)
+        idxs = np.zeros(cb.value.shape)
+        for i in range(cb.value.shape[0]):
+            idx = np.argmin(np.abs(rr.datetime[:].astype('datetime64[ms]') - cb.datetime[i].astype('datetime64[ms]')))
+            idxs[i] = idx
+        cb.rawdata = [rr]
+        cb.rawdata_indices = idxs
+        cb.method = m1
+        cc = d.new(cb)
+        # Read in the raw wind data
+        fn_wd = os.path.join(self.data_dir, 'minidoas', 'wind', '20161101_WD_00.txt')
+        fn_ws = os.path.join(self.data_dir, 'minidoas', 'wind', '20161101_WS_00.txt')
+        e2 = d.read({'direction': fn_wd, 'speed': fn_ws}, ftype='minidoas-wind')
+        gfb = e2['GasFlowBuffer']
+        # Read in the flux estimates
+        e3 = d.read(os.path.join(self.data_dir, 'minidoas', 'NE_2016_11_01_Scans.csv'),
+                    date='2016-11-01', ftype='minidoas-scan')
+        fb = e3['FluxBuffer']
+        dt = fb.datetime[:].astype('datetime64[s]')
+        indices = []
+        for _dt in dt:
+            idx = np.argmin(np.abs(cc.datetime[:].astype('datetime64[us]') - _dt))
+            idx0 = idx
+            while True:
+                angle = rr.inc_angle[cc.rawdata_indices[idx]+1]
+                if angle > 180.:
+                    break
+                idx += 1
+            idx1 = idx
+            indices.append([idx0, idx1])
+        fb.concentration = cc
+        fb.concentration_indices = indices
+        f = d.new(fb)
+        
+        # Now combine the wind speed with the plume direction
+        mb1 = e3['MethodBuffer']
+        gfb1 = e3['GasFlowBuffer']
+        dt = gfb1.datetime[:].astype('datetime64[s]')
+        vx = []
+        vy = []
+        for i, _dt in enumerate(dt):
+            idx = np.argmin(np.abs(gfb.datetime[:].astype('datetime64[s]') - _dt))
+            vx1, vy1 = gfb.vx[idx], gfb.vy[idx]
+            ws = np.sqrt(vx1*vx1 + vy1*vy1)
+            vx2, vy2 = gfb1.vx[i], gfb1.vy[i]
+            wd = vec2bearing(vx2, vy2)
+            vx2, vy2 = bearing2vec(wd, ws)
+            vx.append(vx2)
+            vy.append(vy2)
+        gfb1.vx = vx
+        gfb1.vy = vy
+        gf = d.new(gfb)
+        m1 = d.new(mb1)
+        gfb1.methods = [m1]
+        gf1 = d.new(gfb1) 
+        
+        # Now read in preferred flux values downloaded from FITS
+        data = np.loadtxt(os.path.join(self.data_dir, 'minidoas', 'FITS_NE_20161101.csv'),
+                          dtype=np.dtype([('date','S19'),('val',np.float),('err',np.float)]),
+                          skiprows=1, delimiter=',')
+        dates = data['date'].astype('datetime64[s]')+ np.timedelta64(13,'h')
+        indices = []
+        for i, dt in enumerate(dates):
+            idx = np.argmin(np.abs(f.datetime[:].astype('datetime64[s]') - dt))
+            indices.append(idx)
+        pfb = PreferredFluxBuffer(fluxes=[f],
+                                  flux_indices=[indices],
+                                  value=data['val']*86.4,
+                                  value_error=data['err'],
+                                  datetime=dates.astype(str))
+        d.new(pfb)
+        d.close()
+
+
+def suite():
+    return unittest.makeSuite(MiniDoasPluginTestCase, 'test')
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
